13e39893511327f2925dd9ab12942f70364317ab
kwankiu <ngkwankiuu@gmail.com>
2021-11-11 01:06:18 +0800

    changed background, added user container

--- pages/auth.js
+++ pages/auth.js
@@ -1,11 +1,23 @@
+import * as React from 'react'
 import styles from '../styles/auth.module.css'
+import { Avatar, Button, Zoom } from '@mui/material'
 
 export default function Auth() {
+
+const [showuser,setUser] = React.useState(true);
+
   return (
     <main className={styles.main}>
     <br />
-    <br />
-    <p className={styles.title}>Login</p>
+    <Zoom in={showuser}>
+    <Button className={styles.userContainer} onClick={() => {setUser(false)}}>
+    <div>
+    <br />  
+    <Avatar className={styles.avatar} />
+    <p>User</p>
+    </div>
+    </Button>
+    </Zoom>
   </main>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,8 @@ React.useEffect(() => {
 React.useEffect(() => {
   if (showauth) {
   document.getElementById("auth").style.display = "unset";
-  document.documentElement.style.setProperty('--background', "url('https://cdn.pixabay.com/photo/2021/08/26/10/51/cliff-6575844_1280.jpg')");
+  //document.documentElement.style.setProperty('--background', "url('https://cdn.pixabay.com/photo/2017/06/03/20/12/art-2369664_1280.jpg')");
+  document.documentElement.style.setProperty('--background', "url('https://cdn.pixabay.com/photo/2019/04/26/17/47/color-4158152_1280.jpg')");
   } else {
   document.getElementById("auth").style.display = "none";  
   document.documentElement.style.setProperty('--background', '#282828');
